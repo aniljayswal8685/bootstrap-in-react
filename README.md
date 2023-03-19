3e21e491abc59511588a43867e920bb310690329
aniljayswal8685 <93642023+aniljayswal8685@users.noreply.github.com>
2023-03-19 09:15:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bootstrap-in-react
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-y4hb7x)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import { Button,} from 'react-bootstrap';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Bootstrap in React</h1>
+      <Button>bootstrap button</Button>
+      
+    </div>
+  );
+}
